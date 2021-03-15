c56fbe22fd81e3f029d564c8adcde9b35b598c37
Howard Chang <sir.airpower@gmail.com>
2021-03-15 09:12:50 +0800

    add yb fundation feature
    spring boot 
    gradle

---
+++ src/main/java/com/tudi/yb/YbApplication.java
@@ -0,0 +1,13 @@
+package com.tudi.yb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YbApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/tudi/yb/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.tudi.yb.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @RequestMapping("/first/{name}")
+    public String firstVisit(@PathVariable String name){
+      return "Hello ~ "+ name;
+    }
+
+
+
+}
